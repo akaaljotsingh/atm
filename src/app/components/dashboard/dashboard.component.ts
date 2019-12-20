a587659f801b392a1da0986d9c2d78f5ecbdc5e5
sunitakatoch <sunitakatoch20@gmail.com>
2019-12-20 13:48:51 +0530

    changes in column

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -3,16 +3,16 @@ import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog
 
 export interface PeriodicElement {
   name: string;
-  position: string;
+  position: number;
   weight: number;
   symbol: string;
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
-  {position: status, name: 'Task1 ' , weight: 1.0079, symbol: 'H'},
-  {position: status, name: 'Task2', weight: 4.0026, symbol: 'He'},
-  {position: status, name: 'Task3', weight: 6.941, symbol: 'Li'},
-  {position:status, name: 'Task4', weight: 9.0122, symbol: 'Be'},
+  {position: 1, name: 'Task1 ' , weight: 1.0079, symbol: 'H'},
+  {position: 2, name: 'Task2', weight: 4.0026, symbol: 'He'},
+  {position: 3, name: 'Task3', weight: 6.941, symbol: 'Li'},
+  {position: 4, name: 'Task4', weight: 9.0122, symbol: 'Be'},
   // {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
   // {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
   // {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
