0f0a92c56d8f84a238c91f13176a31adc66cdfc9
baljit7262 <baljit7262@gmail.com>
2020-01-09 12:38:47 +0530

    nothing

--- src/app/shared/activity/activity.component.ts
+++ src/app/shared/activity/activity.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
+
 
 @Component({
   selector: 'app-activity',
@@ -8,7 +10,15 @@ import { Component, OnInit } from '@angular/core';
 export class ActivityComponent implements OnInit {
   checkListSection=false;
   AddMembersSection=false;
-  constructor() { }
+ 
+
+  constructor(
+    public dialogRef: MatDialogRef<ActivityComponent>,
+  ) {}
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
 
   ngOnInit() {
   }
